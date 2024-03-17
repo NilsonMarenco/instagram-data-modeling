100f25cfa16fd7c2bc83636633a25fb809ccf13a
NilsonMarenco <137912131+NilsonMarenco@users.noreply.github.com>
2024-03-17 16:22:22 +0000

    Cambios keys

--- src/models.py
+++ src/models.py
@@ -13,29 +13,33 @@ class User(Base):
     username = Column(String(250), nullable=False)
     firstname = Column(String(250))
     lastname = Column(String(250))
-    email = Column(String(250), primary_key=True)
-
+    email = Column(String(250), unique=True)  
 
 class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(250))
     author_id = Column(Integer, ForeignKey('user.id'))
-    post_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.id'))  
+
+    user = relationship("User")  
+    post = relationship("Post")  
 
 class Follower(Base):
     __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
     user_from_id = Column(Integer, ForeignKey('user.id'))
     user_to_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+
+    user_from = relationship("User", foreign_keys=[user_from_id])  
+    user_to = relationship("User", foreign_keys=[user_to_id])  
 
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+
+    user = relationship("User")  
 
 class Media(Base):
     __tablename__ = 'media'
@@ -43,10 +47,8 @@ class Media(Base):
     type = Column(String)
     url = Column(String(250))
     post_id = Column(Integer, ForeignKey('post.id'))
-    post = relationship(Post)
 
-
-
+    post = relationship("Post")  
     def to_dict(self):
         return {}
 
@@ -55,5 +57,5 @@ try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
